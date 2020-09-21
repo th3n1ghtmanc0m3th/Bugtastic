2a73bfcbf61eeaf3cd5d85d1fba61bd33bcdb583
th3n1ghtmanc0m3th <jaywehrman@gmail.com>
2020-09-21 11:32:59 -0700

    fixes issue #1, compiles cleanly

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
   int age;
-  string name;
-  char last initial;
+  string _name;
+  char last_initial;
 
-  cout<<"Hi. What is your first name? "
-  cin<<name;
-  cout<<"name, what is the first letter of your last name? ";
-  cin>>last initial;
-  cout<<"Thanks, "<<name<< <<last initial<<.<<endl;
-  cout<<"Please also tell me how old you are: ";
-  cin>>age>>endl;
+  cout << "Hi. What is your first name? ";
+  cin >> _name;
+  cout << _name << ", what is the first letter of your last name? ";
+  cin >> last_initial;
+  cout << "Thanks, " << _name << " " << last_initial << "." << endl;
+  cout << "Please also tell me how old you are: ";
+  cin >> age;
 
   if( age < 12 )
   {
-    cout<<"Hey kid, how do you like school?\n";
+    cout << "Hey kid, how do you like school?\n";
   }
   if( age > 11 && age < 18 )
-    cout<<"Cool! How's highschool going?\n";
+    cout << "Cool! How's highschool going?\n";
   else( age >= 18 );
   {
-    cout<<"Pleased to meet you!\n";
+    cout << "Pleased to meet you!\n";
   }
 
   return 0;
